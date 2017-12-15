ed7d6e2dff074f4d41d1cad2939ff7e1c8b89ea8
paas <paas@cse-codegen-backend-4053950211-hj78v>
2017-12-15 10:02:51 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/qwwei/QwweiApplication.java
@@ -0,0 +1,11 @@
+package com.service.qwwei;
+
+import io.servicecomb.foundation.common.utils.BeanUtils;
+import io.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class QwweiApplication {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
